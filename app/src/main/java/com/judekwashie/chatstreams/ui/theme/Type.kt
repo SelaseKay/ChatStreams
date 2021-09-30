3d3eed49515030586087b89bd5b00bb6ce31f1a8
Selase Kwashie <judekwashie70@gmail.com>
2021-09-30 20:42:40 +0000

    Initial commit

---
+++ app/src/main/java/com/judekwashie/chatstreams/ui/theme/Type.kt
@@ -0,0 +1,45 @@
+package com.judekwashie.chatstreams.ui.theme
+
+import androidx.compose.material.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.judekwashie.chatstreams.R
+
+val fonts = FontFamily(
+    Font(R.font.merriweather_bold, weight = FontWeight.Bold),
+    Font(R.font.merriweather_light, weight = FontWeight.Light),
+    Font(R.font.merriweather_regular, weight = FontWeight.Normal)
+)
+
+// Set of Material typography styles to start with
+val Typography = Typography(
+    body1 = TextStyle(
+        fontFamily = fonts,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp
+    ),
+
+    h1 = TextStyle(
+        fontFamily = fonts,
+        fontWeight = FontWeight.Bold,
+        fontSize = 24.sp
+    ),
+
+
+
+    /* Other default text styles to override
+    button = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.W500,
+        fontSize = 14.sp
+    ),
+    caption = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp
+    )
+    */
+)
