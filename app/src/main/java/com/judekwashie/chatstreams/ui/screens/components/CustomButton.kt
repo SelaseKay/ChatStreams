4e2d161063dc29efd0a1b73d0d533e30edbd1fcc
Selase Kwashie <judekwashie70@gmail.com>
2021-10-04 22:05:36 +0000

    SignInButton and NavigationText added

---
+++ app/src/main/java/com/judekwashie/chatstreams/ui/screens/components/CustomButton.kt
@@ -0,0 +1,39 @@
+package com.judekwashie.chatstreams.ui.screens.components
+
+import android.graphics.Color.parseColor
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun CustomButton(buttonText: String, onClick: () -> Unit) {
+    Button(
+        modifier = Modifier
+            .height(32.dp)
+            .padding(
+                start = 32.dp,
+                end = 32.dp,
+            )
+            .fillMaxWidth(),
+        onClick = onClick,
+        elevation = ButtonDefaults.elevation(defaultElevation = 0.dp),
+        contentPadding = PaddingValues(0.dp),
+        colors = ButtonDefaults
+            .buttonColors(
+                backgroundColor = Color(parseColor("#9789EA"))
+            ),
+        shape = RoundedCornerShape(16.dp)
+    ) {
+        Text(
+            text = buttonText,
+            style = MaterialTheme.typography.body2,
+            color = Color.White,
+            textAlign = TextAlign.Center
+        )
+    }
+}
---
+++ app/src/main/java/com/judekwashie/chatstreams/ui/screens/components/NavigationText.kt
@@ -0,0 +1,79 @@
+package com.judekwashie.chatstreams.ui.screens.components
+
+import androidx.compose.foundation.text.ClickableText
+import androidx.compose.material.MaterialTheme
+import android.graphics.Color.parseColor
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun NavigationText(infoText: String, navigationText: String) {
+    val annotatedText = buildAnnotatedString {
+        withStyle(
+            style = SpanStyle(
+                color = Color(parseColor("#858585")),
+                fontWeight = FontWeight.Normal,
+                fontSize = 12.sp,
+                fontStyle = MaterialTheme.typography.body1.fontStyle
+            )
+        ) {
+            append(infoText)
+        }
+
+
+        // We attach this *URL* annotation to the following content
+        // until `pop()` is called
+        pushStringAnnotation(
+            tag = navigationText,
+            annotation = navigationText
+        )
+        withStyle(
+            style = SpanStyle(
+                color = Color(parseColor("#0085FF")),
+                fontWeight = FontWeight.Bold,
+                fontSize = 12.sp,
+                fontStyle = MaterialTheme.typography.body1.fontStyle
+            )
+        ) {
+            append(navigationText)
+        }
+
+        pop()
+    }
+
+    ClickableText(
+        style = TextStyle(
+            textAlign = TextAlign.Right
+        ),
+        modifier = Modifier
+            .padding(
+                top = 24.dp,
+                end = 32.dp
+            )
+            .fillMaxWidth(),
+        text = annotatedText,
+        onClick = { offset ->
+            // We check if there is an *URL* annotation attached to the text
+            // at the clicked position
+            annotatedText.getStringAnnotations(
+                tag = navigationText, start = offset,
+                end = offset
+            )
+                .firstOrNull()?.let { annotation ->
+                    // If yes, we log its value
+
+                }
+        }
+    )
+}
